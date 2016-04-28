4b8f4ad8895a1a4a6eaa34949b0b4de9bd88d471
Ieuan <Ieuan>
2016-04-27 23:43:37 -0400

    Committing WIP, having set up an etsy API key, and added some factories

--- js/View.Controller.js
+++ js/View.Controller.js
@@ -7,6 +7,8 @@ var View = View || {};
 
     "use strict";
 
+    View.api_key = "99z99h6ab0ff4m7bwebzzose";
+
     // View.factories provides methods and data constructs to create new elements for the view based on user inputs
     View.factories = {
         api: {
@@ -17,9 +19,52 @@ var View = View || {};
         },
         // creates view-ready representations from data
         html: {
+            // abstract element builder using native ECMA
+            el: function( options ){
+                var tag = options.tag || false
+                , attrs = options.attrs || false
+                , datum = options.datum || false
+                , el = false;
+                if( tag ){
+                    el = document.createElement( tag );
+                    if( attrs && attrs.length ){
+                        for( var i=0, count=attrs.length; i<count; i+=1 ){
+                            el.setAttribute( attrs[ i ].attr, attrs[ i ].set );
+                        }
+                    }
+                    if( datum ){
+                        // we are passing text
+                        if( typeof datum === "string" ){
+                            var txt = document.createTextNode( datum );
+                            el.appendChild( txt );
+                        }
+                        // we are passing html
+                        else if( typeof datum === "object" ){
+                            el.appendChild( datum );
+                        } else {
+                            el.innerHTML = datum;
+                        }
+                    }
+                }
+                return el;
+            },
             // creates an instance for the view, based on search input and results from the API
             article: function( options ){
-
+                var data = options.data || false
+                , article = false;
+                if( data ){
+                    var article = View.factories.html.el( { tag: "article", datum: data, attrs:[ { attr: "class", set: "result" } ] } );
+                }
+                return article;
+            },
+            // creates a script tag for JSONP facilitation
+            script: function( options ){
+                var src = options.src || false
+                , script = false;
+                if( src ){
+                    var script = View.factories.html.el( { tag: "script", attrs:[ { attr: "src", set: src } ] } );
+                }
+                return script;
             }
         }
     }
